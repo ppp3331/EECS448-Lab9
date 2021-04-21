feb8745cf9747c9fb7ee76727d227446851fb7d5
Becky Lin <beckylin123@ku.edu>
2021-04-21 14:59:49 -0500

    1

---
+++ Exercise1.php
@@ -0,0 +1,23 @@
+<?php
+function mult($x, $y) {
+  $z = $x * $y;
+  return $z;
+}
+
+echo "<tr>";
+for ($i = 1; $i <= 100; $i++)
+{
+  echo "<th>". $i . "</th>";
+}
+echo "</tr>";
+
+for ($i = 1; $i <= 100; $i++)
+{
+  echo "<tr><th>" . $i . "</th>";
+  for ($j = 1; $j <= 100; $j++)
+  {
+    echo "<td>" . mult($i, $j) . "</td>";
+  }
+  echo "</tr>";
+}
+?>
